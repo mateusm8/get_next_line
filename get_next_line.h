9a612a6a5211600cec44b456c6239c26a1106412
mateusm8 <93933608+mateusm8@users.noreply.github.com>
2022-03-29 18:23:03 +0100

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmagalha <mmagalha@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/29 16:55:20 by mmagalha          #+#    #+#             */
+/*   Updated: 2022/03/29 18:07:43 by mmagalha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *c);
+char	*ft_strldup(char *s1, int size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_line(char *buffer);
+char	*ft_next_line(char *buffer);
+char	*ft_read_fd(int fd, char *buffer);
+
+#endif
